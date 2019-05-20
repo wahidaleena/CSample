254fd544db441e83f3e48644983ee908e1422db9
wahidaleena <34679809+wahidaleena@users.noreply.github.com>
2019-05-20 14:32:56 +0530

    Update sample.cmake

--- sample.cmake
+++ sample.cmake
@@ -3,4 +3,5 @@ cmake_minimum_required(VERSION 2.6)
 	add_executable(	calculator calculator.c	)
 	add_executable(	convert convert.c	)
 	add_executable(	word_count word_count.c	)
+	add_executable(	mysql mysql.c	)
 	
